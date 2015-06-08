475b3cc89d7c4a6830f292fe4939bbd132f7416d
OnlyRefat <sniperefat@gmail.com>
2015-06-08 14:58:17 +0600

    Update Device.js

--- js/controller/Device.js
+++ js/controller/Device.js
@@ -1,18 +1,13 @@
 app.controller('deviceCtrl', function($scope,$http) {
-	$scope.developers = [
-	    {"name":"John", "family":"Doe"}, 
-	    {"name":"Anna", "family":"Smith"},
-	    {"name":"Peter", "family":"Jones"},
-	    {"name":"Alex", "family":"Volkov"}, 
-	    {"name":"Yaniv", "family":"Smith"},
-	]
+	$scope.developers = []
 
 
 $http.get('http://phoneapi-disl.rhcloud.com/PhoneDetails/').
   success(function(data, status, headers, config) {
-  	
-  	console.log(data);
+
 $scope.developers=data;
+
+  console.log($scope.developers);
   }).
   error(function(data, status, headers, config) {
     // called asynchronously if an error occurs
