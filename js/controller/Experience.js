eac246ba3963c5dab74c0b5dca9852bd893c7c0c
OnlyRefat <sniperefat@gmail.com>
2016-09-25 00:58:06 +0600

    experience updated

--- js/controller/Experience.js
+++ js/controller/Experience.js
@@ -28,6 +28,7 @@ app.controller('experienceCtrl', function($scope) {
   {title:'Position', content:'13th'}
   ]
   $scope.careerInfo=[  {title:'RedqTeam(October\'15-Present)',content:'Working as a Software Developer.'},
+  {title:'Upwork(June\'15-Present)',content:'Working as a Freelancer(software Developer).'},
   {title:'Dhrubok Info Tech Ltd(March\'15-September\'15)',content:'Worked as a Software Developer.'},
   {title:'Monist IT Inc(April\'14-September\'14)',content:'Worked as an Intern.'}]
 
