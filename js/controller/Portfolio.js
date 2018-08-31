76552ec7159275da8a3e8d2a0cb5a2d314571c58
OnlyRefat <sniperefat@gmail.com>
2018-08-31 16:22:40 -0400

    some skiils added

--- js/controller/Portfolio.js
+++ js/controller/Portfolio.js
@@ -1,7 +1,7 @@
 app.controller('portfolioCtrl',function($scope){
 
 	$scope.project = [{name:'Reactive Pro',details:"wordpress Searching & Grid Building Plugin on Codecanyon(RedQ Inc Production).", image:['image/RP/1.png'],link:'https://codecanyon.net/item/reactive-pro-advanced-wordpress-search-filtering-grid/17425763?s_rank=5'},
-	{name:'Latify',details:"An Sync Google Map Reactjs Implementation for finding latitude, longitude. Open Source", image:['image/RP/2.png'],link:'https://onlyrefat.github.io/latify/'},
+	{name:'Latify',details:"An Google Map Reactjs Implementation for finding latitude, longitude. Open Source", image:['image/RP/2.png'],link:'https://onlyrefat.github.io/latify/'},
 	{name:'latify-leaflet',details:"An Sync Leaflet Map Reactjs Implementation for finding latitude, longitude. Open Source", image:['image/RP/2.png'],link:'https://onlyrefat.github.io/latify-leaflet/'},
 	{name:'Hacker Rank Problems',details:"Solving Problems to get proper control of a particular laguage like Python. 30+ problems solved using python", image:['image/default.png'],link:'https://www.hackerrank.com/Refat_Rafi'},
 	{name:'UVA Problems',details:"In my academic years i have solved a lot of problems on the UVA, LightOJ, Code force. Out of them 150 on UVA. Here is the link of my uhunt link in the project button. That helpped me to be a better programmer.", image:['image/UVA/1.jpg','image/UVA/2.jpg','image/UVA/3.jpg'],link:'http://uhunt.felix-halim.net/id/67139'},
