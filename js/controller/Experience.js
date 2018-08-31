42d008c3676c704311d854a11ef16a4884717d7f
OnlyRefat <sniperefat@gmail.com>
2018-08-31 16:14:32 -0400

    some changes

--- js/controller/Experience.js
+++ js/controller/Experience.js
@@ -27,7 +27,7 @@ app.controller('experienceCtrl', function($scope) {
   {title:'CGPA', content:'3.49(Out of 4)'},
   {title:'Position', content:'13th'}
   ];
-  $scope.careerInfo=[  {title:'RedQ Inc(October\'15-Present)',content:'Working as a Software Developer.'},
+  $scope.careerInfo=[  {title:'RedQ Inc(October\'15-July, 2018)',content:'Working as a Software Developer.'},
   {title:'Upwork(June\'15-Present)',content:'Working as a Freelancer(software Developer).'},
   {title:'Dhrubok Info Tech Ltd(March\'15-September\'15)',content:'Worked as a Software Developer.'},
   {title:'Monist IT Inc(April\'14-September\'14)',content:'Worked as an Intern.'}]
--- js/controller/Profile.js
+++ js/controller/Profile.js
@@ -1,6 +1,6 @@
 app.controller('profileCtrl', function($scope) {
 // $scope.des='I am, in a nutshell, a big geek who works in web development for more than 3 Years and now. Basically i love to work with complex ideas and turn them into nice working applications. You can call me a good problem solver. In my academic years, I have spent most of the time, solving problems in the online judge like UVA, LightOj, Codeforces. \n\n I am mainly fully focused on Javascript. I generally prefer React.js/ Angularjs for the frontend purpose along with the basic html/ css and Nodejs for backend purpose. In my free time i like to spend my time at StackOverflow (1050+ Reputaions). In terms of my job, i am so much professional . I have 100% successful job complitaion on Upwork till now with more than 200+ working hours, as well as some fixed price jobs. You can have a view by clicking the bellow links. \n \n';
-$scope.des='Currently, I am a Software Engineer who works for a reputed startup in Bangladesh working for more than 3 years and now. In my academic years, I have devoted myself most of the times solving complex Online problem on the Online sites like (codeforce, Uva). I am a Computer Science Graduate, graduated from an Engineering University (Rajshahi University of Engineering & Technology, Bangladesh) in the year 2014. As learning is my passion, I love to work with the latest technologies.My Current focus is on Machine learning as the word is entering in the world of AI. As part of my development, I have finished an online course on Machine Learning titled "Machine Learning by Stanford University" on Coursera. And currently solving Projects on Kaggle for implementing those theoretical Concepts. Besides this, on my free time i like to engage myself for open source contributions. I have already gained 1000+ reputations on Stackoverflow and have several projects on github. The links are given bellow.';
+$scope.des='Currently, I am a Phd Student of Electrical and Computer Engineering studying in University of Michigan-Dearborn. After working for nearly 4 years in the software industry, I have decided to take a new challenge in my career. In my leisure, I like to spent my time by solving complex problems and contributing to open source projects.';
 // - Mean Stack, Node.js, Express.js, loopback, Sails.js; MongoDB,MySql;  
 // - CSS(3); 
 // - HTML(5), Twitter Bootstrap;  
@@ -17,13 +17,13 @@ $scope.image="Here will be the image.";
 $scope.biodata=[{field:'Name',value:'Rafi Ud Daula Refat'},
 {field:'Born',value:'4th January, 1993'},
 {field:'Blood Group',value:'O(+ve)'},
-{field:'Location',value:'Mohammadpur,Dhaka'}];
+{field:'Location',value:'Dearborn, Michigan, USA'}];
 $scope.skilldata=[{field:'Favt Languages',value:'Js, Python'},
 {field:'Librabies',value:'React, Redux, Redux-saga'},
 {field:'Build Tools',value:'Gulp, Webpack'},
 {field:'FrameWork',value:'Mean, Loopback, Sails.js'},
 {field:'Database',value:'MongoDB, MySQL'},
-{field:'Research Interest',value:'Machine Learning, AI, Data Science'},]
+{field:'Research Interest',value:'Machine Learning, AI, Data Science, Cyber Software Security'},]
 });
 //<a href="http://stackoverflow.com/users/2040589/rafi-ud-daula-refat">Stackoverflow</a>
 //<a href="https://www.upwork.com/freelancers/~01191deb53e8d6d358">Upwork</a>
